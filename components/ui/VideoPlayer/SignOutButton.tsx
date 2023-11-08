e9295613a221cb37fad0352b0eebf4c3d6f26053
Noah Milberger <n.milberger@gmail.com>
2023-11-08 12:43:02 -0500

    Fixed incorrect import for Button component in SignOutButton.

--- components/ui/VideoPlayer/SignOutButton.tsx
+++ components/ui/VideoPlayer/SignOutButton.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import Button from '../Button';
+import Button from '../Input/Button';
 import { useSupabase } from '@/app/supabase-provider';
 import { useRouter } from 'next/navigation';
 
