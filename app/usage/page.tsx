d37bf69ec4c4db15a9a0d2b0439eb9c76304a58d
Noah Milberger <n.milberger@gmail.com>
2023-11-08 11:27:32 -0500

    Abstracted usage page.

--- app/usage/page.tsx
+++ app/usage/page.tsx
@@ -1,98 +1,27 @@
 import { getSession, getJobs } from '../supabase-server';
-import StatusTag from '@/components/ui/StatusTag';
-import { Job } from '@/types_db';
-import {
-  Button,
-  Flex,
-  Stack,
-  Table,
-  TableContainer,
-  Tbody,
-  Td,
-  Text,
-  Th,
-  Thead,
-  Tooltip,
-  Tr
-} from '@chakra-ui/react';
-import { DateTime } from 'luxon';
-import Link from 'next/link';
+import UsageTable from '@/components/feature-usage/ui/UsageTable';
+import PageHeader from '@/components/ui/Display/PageHeader';
+import { Flex, Stack } from '@chakra-ui/react';
 import { redirect } from 'next/navigation';
 
-function NoUsageView() {
-  return (
-    <Stack alignItems="center" gap={4}>
-      <Text
-        fontSize="2xl"
-        fontWeight="medium"
-      >{`You haven't translated any videos yet`}</Text>
-      <Button>
-        <Link href="/">Translate your first video</Link>
-      </Button>
-    </Stack>
-  );
-}
-
-function UsageTable({ jobs }: { jobs: Job[] }) {
-  return (
-    <TableContainer>
-      <Table colorScheme={'whiteAlpha'}>
-        <Thead>
-          <Tr>
-            <Th>Date</Th>
-            <Th>Status</Th>
-            <Th isNumeric>Credits</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {jobs?.map((job: Job) => {
-            const createdAt = DateTime.fromJSDate(
-              new Date(job.created_at)
-            ).toFormat("MMM. d, yyyy 'at' h:mm a");
-            const createdDate = DateTime.fromJSDate(
-              new Date(job.created_at)
-            ).toFormat('MMM. d, yyyy');
-            return (
-              <Tr key={job.id}>
-                <Td>
-                  <Tooltip hasArrow label={createdAt}>
-                    {createdDate}
-                  </Tooltip>
-                </Td>
-                <Td>
-                  <StatusTag status={job.status!} />
-                </Td>
-                <Td isNumeric>{job.credits}</Td>
-              </Tr>
-            );
-          })}
-        </Tbody>
-      </Table>
-    </TableContainer>
-  );
-}
-
 export default async function Usage() {
+  // Grab data from db
   const session = await getSession();
   const jobs = await getJobs(session?.user.id as string);
 
+  // Redirect user to signin if not signed in
   if (!session) {
     return redirect('/signin');
   }
 
+  // Page content
+  const title = `Usage`;
+
   return (
     <Flex w="full" px={4} justifyContent={'center'} color="white">
-      <Stack w="full" maxW="4xl" py={8} gap={0}>
-        <Text
-          textAlign={'start'}
-          fontSize={['4xl', '6xl']}
-          fontWeight="extrabold"
-          color="white"
-          className="sm:text-center"
-        >
-          Usage
-        </Text>
-        {jobs && jobs.length ? <UsageTable jobs={jobs} /> : NoUsageView()}
+      <Stack w="full" maxW="4xl" py={8} gap={8}>
+        <PageHeader title={title} />
+        <UsageTable jobs={jobs} />
       </Stack>
     </Flex>
   );
