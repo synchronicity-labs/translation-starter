e79fcb55e4e68d6f9a6c95da84ccf5cf5ffcc141
Noah Milberger <n.milberger@gmail.com>
2023-11-10 09:02:37 -0500

    Updated next config file to fix issue with ffmpeg.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,21 @@
+const webpack = require('webpack');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true,
     serverActions: true
+  },
+  webpack: (config) => {
+    // Define the `FLUENTFFMPEG_COV` variable as false
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.FLUENTFFMPEG_COV': false
+      })
+    );
+
+    return config;
   }
 };
 
