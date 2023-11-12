5b35ed7593ca483855ef5e55a0bdba5a6d3f43e8
Noah Milberger <n.milberger@gmail.com>
2023-11-11 21:27:36 -0500

    Updated upload-to-s3 api route so it uploads video then transcodes video to audio and uploads audio

--- app/api/aws/upload-to-s3/route.ts
+++ app/api/aws/upload-to-s3/route.ts
@@ -1,60 +1,10 @@
-// import { insertBeforeDot } from '@/utils/helpers';
-// import { S3 } from 'aws-sdk';
-// import { NextRequest, NextResponse } from 'next/server';
-// import { v4 as uuidv4 } from 'uuid';
-
-// // Initialize S3 instance
-// const s3 = new S3({
-//   accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY,
-//   secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY,
-//   region: process.env.NEXT_PUBLIC_S3_REGION
-// });
-
-// export async function POST(request: NextRequest) {
-//   const data = await request.formData();
-//   const file: File | null = data.get('file') as unknown as File;
-
-//   if (!file) {
-//     return NextResponse.json({ success: false, message: `No file found` });
-//   }
-
-//   const bytes = await file.arrayBuffer();
-//   const buffer = Buffer.from(bytes);
-
-//   const uuid = uuidv4();
-
-//   const params = {
-//     Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME as string,
-//     Key: insertBeforeDot(`/translation-test-input/${file.name}`, uuid),
-//     Body: buffer
-//   };
-
-//   let videoUpload: S3.ManagedUpload.SendData | undefined;
-
-//   try {
-//     videoUpload = await s3.upload(params).promise();
-//     return NextResponse.json({
-//       success: true,
-//       data: { videoUrl: videoUpload.Location }
-//     });
-//   } catch (error) {
-//     console.error(`Failed to upload video to AWS S3`, error);
-//     return NextResponse.json({
-//       success: false,
-//       message: `Failed to upload video to AWS S3`,
-//       error
-//     });
-//   }
-// }
-
-import { insertBeforeDot, replaceFileExtension } from '@/utils/helpers';
 import { S3 } from 'aws-sdk';
 import ffmpeg from 'fluent-ffmpeg';
 import { createWriteStream, readFileSync } from 'fs';
+import { promises as fsPromises } from 'fs';
 import { NextRequest, NextResponse } from 'next/server';
-import { tmpdir } from 'os';
+import fetch from 'node-fetch';
 import path from 'path';
-import { Readable } from 'stream';
 import { v4 as uuidv4 } from 'uuid';
 
 // Initialize S3 instance
@@ -65,129 +15,106 @@ const s3 = new S3({
 });
 
 export async function POST(request: NextRequest) {
-  const data = await request.formData();
-  const file: File | null = data.get('file') as unknown as File;
-
-  if (!file) {
-    return NextResponse.json({ success: false, message: `No file found` });
+  if (request.method !== 'POST') {
+    return NextResponse.json({ success: false, message: `Method not allowed` });
   }
 
-  const bytes = await file.arrayBuffer();
-  const buffer = Buffer.from(bytes);
+  const data = await request.formData();
+  console.log('data: ', data);
+  const videoFile: File | null = data.get('file') as unknown as File;
+  console.log('videoFile: ', videoFile);
 
-  const uuid = uuidv4();
-
-  const params = {
-    Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME as string,
-    Key: insertBeforeDot(`/translation-test-input/${file.name}`, uuid),
-    Body: buffer
-  };
-
-  let videoUpload: S3.ManagedUpload.SendData | undefined;
-
-  try {
-    videoUpload = await s3.upload(params).promise();
-  } catch (error) {
-    console.error(`Failed to upload video to AWS S3`, error);
+  if (!videoFile) {
     return NextResponse.json({
       success: false,
-      message: `Failed to upload video to AWS S3`,
+      message: `No file found in request`
+    });
+  }
+
+  const uuid = uuidv4();
+  let videoUrl: string;
+  let audioUrl: string;
+
+  const buffer = Buffer.from(await videoFile.arrayBuffer());
+
+  // Video upload
+  try {
+    const params = {
+      Bucket: 'synchlabs-public',
+      Key: `/translation-test-input/input-video-${uuid}.mp4`, // This is what the file will be named in S3
+      Body: buffer
+    };
+
+    const videoUpload = await s3.upload(params).promise();
+    videoUrl = videoUpload.Location;
+  } catch (error) {
+    console.error('Error uploading file to S3:', error);
+    return NextResponse.json({
+      success: false,
+      message: `Error uploading video file to S3`,
       error
     });
   }
 
-  const stream = Readable.from(buffer);
+  // Create temporary directory and files for video and audio
+  const tempDir = path.resolve('./temp');
+  await fsPromises.mkdir(tempDir, { recursive: true });
 
-  ffmpeg(stream).on('end', async () => {
-    console.log('Finished converting video to audio');
-  });
+  const tempVideoPath = path.join(tempDir, `input-video-${uuid}.mp4`);
+  const tempAudioPath = path.join(tempDir, `input-audio-${uuid}.mp3`);
+
+  // Download video from S3
+  const response = await fetch(videoUrl);
+
+  if (!response.ok)
+    throw new Error(`Failed to fetch video: ${response.statusText}`);
+
+  // Write video to temp file
+  const fileStream = createWriteStream(tempVideoPath);
+  response.body?.pipe(fileStream);
+
+  // Convert video to audio and upload to S3
+  try {
+    audioUrl = await new Promise<string>((resolve, reject) => {
+      fileStream.on('finish', async function () {
+        ffmpeg(tempVideoPath)
+          .toFormat('mp3')
+          .on('end', async () => {
+            console.log('Finished converting video to audio');
+
+            const audioData = readFileSync(tempAudioPath);
+
+            const params = {
+              Bucket: 'synchlabs-public',
+              Key: `/translation-test-input/input-audio-${uuid}.mp3`, // This is what the file will be named in S3
+              Body: audioData
+            };
+
+            const audioUpload = await s3.upload(params).promise();
+
+            // Clean up temp files and directory
+            await Promise.all([
+              fsPromises.unlink(tempVideoPath),
+              fsPromises.unlink(tempAudioPath)
+            ]);
+            await fsPromises.rm(tempDir, { recursive: true, force: true });
+
+            resolve(audioUpload.Location);
+          })
+          .on('error', (error) => {
+            console.error('Error converting video to audio:', error);
+            reject(error);
+          })
+          .saveToFile(tempAudioPath);
+      });
+    });
+    return NextResponse.json({ success: true, data: { videoUrl, audioUrl } });
+  } catch (error) {
+    console.error('Error converting video to audio:', error);
+    return NextResponse.json({
+      success: false,
+      message: `Error converting video to audio`,
+      error
+    });
+  }
 }
-
-// import { insertBeforeDot, replaceFileExtension } from '@/utils/helpers';
-// import { S3 } from 'aws-sdk';
-// import ffmpeg from 'fluent-ffmpeg';
-// import { createWriteStream, readFileSync } from 'fs';
-// import { NextRequest, NextResponse } from 'next/server';
-// import path from 'path';
-// import { Readable } from 'stream';
-// import { v4 as uuidv4 } from 'uuid';
-
-// // Initialize S3 instance
-// const s3 = new S3({
-//   accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY,
-//   secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY,
-//   region: process.env.NEXT_PUBLIC_S3_REGION
-// });
-
-// export async function POST(request: NextRequest) {
-//   const data = await request.formData();
-//   const file: File | null = data.get('file') as unknown as File;
-
-//   if (!file) {
-//     return NextResponse.json({ success: false, message: `No file found` });
-//   }
-
-//   const bytes = await file.arrayBuffer();
-//   const buffer = Buffer.from(bytes);
-//   const videoStream = new Readable({
-//     read() {
-//       this.push(buffer);
-//     }
-//   });
-
-//   const uuid = uuidv4();
-//   const videoFileName = insertBeforeDot(`${file.name}`, uuid);
-//   const audioFileName = replaceFileExtension(videoFileName, '.mp3');
-
-//   const params = {
-//     Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME as string,
-//     Key: videoFileName,
-//     Body: buffer
-//   };
-
-//   let videoUpload: S3.ManagedUpload.SendData | undefined;
-
-//   try {
-//     videoUpload = await s3.upload(params).promise();
-//   } catch (error) {
-//     console.error(`Failed to upload video to AWS S3`, error);
-//     return NextResponse.json({
-//       success: false,
-//       message: `Failed to upload video to AWS S3`,
-//       error
-//     });
-//   }
-
-//   const videoUrl = videoUpload.Location;
-//   const tempDir = path.resolve('./temp');
-//   const tempVideoFilePath = path.join(tempDir, videoFileName);
-//   const tempAudioFilePath = path.join(tempDir, audioFileName);
-
-//   // const videoStream = createWriteStream(videoUrl);
-//   videoStream.on('finish', async () => {
-//     ffmpeg(tempVideoFilePath)
-//       .toFormat('mp3')
-//       .on('end', async () => {
-//         const audioData = readFileSync(tempAudioFilePath);
-//         const params = {
-//           Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME as string,
-//           Key: audioFileName,
-//           Body: audioData
-//         };
-//         try {
-//           const audioUrl = (await s3.upload(params).promise()).Location;
-//           return NextResponse.json({
-//             success: true,
-//             data: { videoUrl, audioUrl }
-//           });
-//         } catch (error) {
-//           return NextResponse.json({
-//             success: false,
-//             message: `Failed to upload audio from video to AWS S3`,
-//             error
-//           });
-//         }
-//       })
-//       .save(tempAudioFilePath);
-//   });
-// }
