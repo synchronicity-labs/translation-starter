b7f7791fc879cadd9726250e61db6417900e8726
Noah Milberger <n.milberger@gmail.com>
2023-11-13 14:04:15 -0500

    Created lip-sync route.

---
+++ app/api/lip-sync/route.ts
@@ -0,0 +1,40 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req: Request) {
+  if (req.method !== 'POST') {
+    return new Response('Method Not Allowed', {
+      headers: { Allow: 'POST' },
+      status: 405
+    });
+  }
+  const { videoUrl, audioUrl } = await req.json();
+
+  const response = await fetch(`https://api.synclabs.so/video`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-API-KEY': process.env.SYNC_LABS_API_KEY as string
+    },
+    body: JSON.stringify({
+      audioUrl,
+      videoUrl,
+      synergize: true
+    })
+  });
+
+  if (!response.ok || !response.body) {
+    console.error(
+      `Failed to lip sync video to audio: ${response.status} ${response.statusText}`
+    );
+    return new Response(JSON.stringify({ error: { statusCode: 500 } }), {
+      status: 500
+    });
+  }
+
+  const data = await response.json();
+  console.log('data: ', data);
+
+  return new Response(JSON.stringify({ data }), {
+    status: 200
+  });
+}
