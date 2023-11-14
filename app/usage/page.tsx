9c3e016dec67e5cf0c752d3c3f0836e64fe9c59f
Noah Milberger <n.milberger@gmail.com>
2023-11-14 07:38:00 -0500

    Removed logic for grabbing jobs and passing them to UsageTable usage page.

--- app/usage/page.tsx
+++ app/usage/page.tsx
@@ -1,4 +1,4 @@
-import { getSession, getJobs } from '../supabase-server';
+import { getSession } from '../supabase-server';
 import UsageTable from '@/components/feature-usage/ui/UsageTable';
 import PageHeader from '@/components/ui/Display/PageHeader';
 import { Flex, Stack } from '@chakra-ui/react';
@@ -7,7 +7,6 @@ import { redirect } from 'next/navigation';
 export default async function Usage() {
   // Grab data from db
   const session = await getSession();
-  const jobs = await getJobs(session?.user.id as string);
 
   // Redirect user to signin if not signed in
   if (!session) {
@@ -21,7 +20,7 @@ export default async function Usage() {
     <Flex w="full" px={4} justifyContent={'center'} color="white">
       <Stack w="full" maxW="4xl" py={8} gap={8}>
         <PageHeader title={title} />
-        <UsageTable jobs={jobs} />
+        <UsageTable />
       </Stack>
     </Flex>
   );
