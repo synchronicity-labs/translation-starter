d4e88f7b364d950fc97c2032d7ff48dbd6725726
Noah Milberger <n.milberger@gmail.com>
2023-11-14 22:23:17 -0500

    Updated logic in api/transcode route to upload audio file to supabase instead of aws s3.

--- app/api/transcode/route.ts
+++ app/api/transcode/route.ts
@@ -1,17 +1,11 @@
-import { S3 } from 'aws-sdk';
+// import supabase from '@/utils/supabase';
+import supabase from '@/utils/supabase';
 import ffmpeg from 'fluent-ffmpeg';
 import { createWriteStream, promises as fsPromises, readFileSync } from 'fs';
 import { NextResponse } from 'next/server';
 import fetch from 'node-fetch';
 import path from 'path';
 import { v4 as uuidv4 } from 'uuid';
-
-// Initialize S3 instance
-const s3 = new S3({
-  accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY,
-  secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY,
-  region: process.env.NEXT_PUBLIC_S3_REGION
-});
 
 export async function POST(req: Request) {
   if (req.method !== 'POST') {
@@ -43,7 +37,7 @@ export async function POST(req: Request) {
 
   // Convert video to audio and upload to S3
   try {
-    audioUrl = await new Promise<string>((resolve, reject) => {
+    const audioBuffer = await new Promise<String>((resolve, reject) => {
       fileStream.on('finish', async () => {
         ffmpeg(tempVideoPath)
           .toFormat('mp3')
@@ -52,13 +46,28 @@ export async function POST(req: Request) {
 
             const audioData = readFileSync(tempAudioPath);
 
-            const params = {
-              Bucket: 'synchlabs-public',
-              Key: `/translation-test-input/input-audio-${uuid}.mp3`, // This is what the file will be named in S3
-              Body: audioData
-            };
+            const filePath = `public/input-audio-${Date.now()}.mp3`;
 
-            const audioUpload = await s3.upload(params).promise();
+            const { data, error } = await supabase.storage
+              .from('translation')
+              .upload(filePath, audioData, {
+                contentType: 'audio/mp3',
+                upsert: false
+              });
+
+            if (error) {
+              console.error('Error uploading audio to S3:', error);
+              reject(error);
+            }
+
+            if (!data) {
+              console.error('No data returned from S3');
+              reject('No data returned from S3');
+            }
+
+            const url = `${
+              process.env.NEXT_PUBLIC_SUPABASE_URL
+            }/storage/v1/object/public/translation/${data!.path}`;
 
             // Clean up temp files and directory
             await Promise.all([
@@ -67,7 +76,8 @@ export async function POST(req: Request) {
             ]);
             await fsPromises.rm(tempDir, { recursive: true, force: true });
 
-            resolve(audioUpload.Location);
+            // resolve(audioUpload.Location);
+            resolve(url);
           })
           .on('error', (error) => {
             console.error('Error converting video to audio:', error);
@@ -76,9 +86,11 @@ export async function POST(req: Request) {
           .saveToFile(tempAudioPath);
       });
     });
+
+    console.log('audioBuffer: ', audioBuffer);
     return NextResponse.json({
       success: true,
-      data: audioUrl
+      data: audioBuffer
     });
   } catch (error) {
     console.error('Error converting video to audio:', error);
