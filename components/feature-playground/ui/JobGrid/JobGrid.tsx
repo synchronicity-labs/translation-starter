9eb019a745ec8f0176259085d284bb43030244a8
Noah Milberger <n.milberger@gmail.com>
2023-11-15 15:57:22 -0500

    Commented out logic for grabbing jobs in realtime.

--- components/feature-playground/ui/JobGrid/JobGrid.tsx
+++ components/feature-playground/ui/JobGrid/JobGrid.tsx
@@ -1,14 +1,16 @@
 'use client';
 
 import RealTimeJobGrid from './RealTimeJobGrid';
+import { Job } from '@/types_db';
 import supabase from '@/utils/supabase';
 
 export default async function JobGrid({ userId }: { userId: string }) {
-  const { data } = await supabase
-    .from('jobs')
-    .select('*')
-    .eq('user_id', userId)
-    .neq('is_deleted', true);
+  // const { data } = await supabase
+  //   .from('jobs')
+  //   .select('*')
+  //   .eq('user_id', userId)
+  //   .neq('is_deleted', true);
 
+  const data: Job[] = [];
   return <RealTimeJobGrid data={data || []} />;
 }
--- components/feature-usage/ui/UsageTable/UsageTable.tsx
+++ components/feature-usage/ui/UsageTable/UsageTable.tsx
@@ -1,13 +1,16 @@
 'use client';
 
 import RealTimeUsageTable from './RealTimeUsageTable';
+import { Job } from '@/types_db';
 import supabase from '@/utils/supabase';
 
 export default async function UsageTable({ userId }: { userId: string }) {
-  const { data } = await supabase
-    .from('jobs')
-    .select('*')
-    .eq('user_id', userId);
+  // const { data } = await supabase
+  //   .from('jobs')
+  //   .select('*')
+  //   .eq('user_id', userId);
+
+  const data: Job[] = [];
 
   return <RealTimeUsageTable data={data || []} />;
 }
