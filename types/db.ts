68ee92c614ee1b645229f9ab965feda5b2b46e95
Noah Milberger <n.milberger@gmail.com>
2023-11-16 21:52:09 -0500

    Updated types.

---
+++ types/db.ts
@@ -0,0 +1,35 @@
+import { Database } from '@/types_db';
+
+export type Subscription = Database['public']['Tables']['subscriptions']['Row'];
+export type Product = Database['public']['Tables']['products']['Row'];
+export type Price = Database['public']['Tables']['prices']['Row'];
+export type Job = Database['public']['Tables']['jobs']['Row'];
+export type JobStatus = Database['public']['Enums']['job_status'];
+export type BillingInterval = 'lifetime' | 'year' | 'month';
+
+export type Transcript = {
+  words: {
+    word: string;
+    time_begin: number;
+    time_end: number;
+    confidence: number;
+  }[];
+  language: string;
+  transcription: string;
+  confidence: number;
+  time_begin: number;
+  time_end: number;
+  speaker: string;
+  channel: string;
+}[];
+
+export interface ProductWithPrices extends Product {
+  prices: Price[];
+}
+export interface PriceWithProduct extends Price {
+  products: Product | null;
+}
+
+export interface SubscriptionWithProduct extends Subscription {
+  prices: PriceWithProduct | null;
+}
