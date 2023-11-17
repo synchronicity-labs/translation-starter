94ac55941f9a11f50a1b6dd579bac2d9826bc8b5
Noah Milberger <n.milberger@gmail.com>
2023-11-16 21:36:26 -0500

    Updated create-job api route to not update creditsToDeduct or originalVideoUrl.

--- app/api/db/create-job/route.ts
+++ app/api/db/create-job/route.ts
@@ -1,52 +1,18 @@
 import { insertJob } from '@/app/supabase-server';
 import { NextResponse } from 'next/server';
 
-// async function getMediaDuration(url: string): Promise<number> {
-//   try {
-//     return new Promise((resolve, reject) => {
-//       ffmpeg.ffprobe(url, (err, metadata) => {
-//         if (err) {
-//           reject(err);
-//         } else if (metadata.format.duration !== undefined) {
-//           resolve(metadata.format.duration);
-//         } else {
-//           reject(new Error('Duration is undefined'));
-//         }
-//       });
-//     });
-//   } catch (error) {
-//     return Promise.reject(error);
-//   }
-// }
-
-// TODO: Fix logic for getting video duration
 export async function POST(req: Request) {
   if (req.method !== 'POST') {
     return NextResponse.json({ success: false, message: `Method not allowed` });
   }
 
-  const { userId, originalVideoUrl } = await req.json();
-
   try {
-    console.log('creating job');
-    console.log('userId: ', userId);
-    console.log('originalVideoUrl: ', originalVideoUrl);
-    // const duration = await getMediaDuration(originalVideoUrl);
-    const duration = 0;
-    const creditsToDeduct = Math.round(duration);
-
-    const jobs = await insertJob(
-      originalVideoUrl,
-      creditsToDeduct,
-      'processing'
-    );
-
-    console.log('jobs: ', jobs);
+    const jobs = await insertJob();
 
     if (!jobs || jobs.length === 0) {
       return NextResponse.json({
         success: false,
-        message: `Error creating job`
+        message: `Error craeting job`
       });
     }
 
