ddf5cc7397761354dbd053f26d2f0f0d8a7f9613
Noah Milberger <n.milberger@gmail.com>
2023-11-18 01:31:58 -0500

    Fixed bug with update-job util function.

--- utils/update-job.ts
+++ utils/update-job.ts
@@ -1,4 +1,3 @@
-import apiRequest from './api-request';
 import { Job, OnFailedJob } from '@/types/db';
 
 export default async function updateJob(
@@ -6,15 +5,19 @@ export default async function updateJob(
   updatedFields: any,
   onFail: OnFailedJob
 ) {
-  try {
-    const path = '/api/db/update-job';
-    await apiRequest(path, {
-      method: 'POST',
-      body: { jobId: job.id, updatedFields }
-    });
-  } catch (error) {
-    const errorMessage =
-      (error as Error).message || 'An unknown error occurred';
-    onFail(job.id, errorMessage);
+  console.log('in update-job.ts');
+  const path = '/api/db/update-job';
+  const updateJob = await fetch(path, {
+    method: 'POST',
+    body: JSON.stringify({
+      jobId: job.id,
+      updatedFields
+    })
+  });
+  if (!updateJob.ok) {
+    onFail(job.id, `Failed to update job`);
+    return;
   }
+
+  console.log('job updated: ', updatedFields);
 }
