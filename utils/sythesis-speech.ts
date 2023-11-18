56ebb33e8b861c20e7daac6a5335ae4ec06a7013
Noah Milberger <n.milberger@gmail.com>
2023-11-18 17:55:11 -0500

    Updated synthesis-speach to use job.voice_id instead of random voice_id.

--- utils/sythesis-speech.ts
+++ utils/sythesis-speech.ts
@@ -8,7 +8,7 @@ export default async function synthesisSpeech(job: Job, onFail: OnFailedJob) {
     const path = '/api/speech-synthesis';
     const synthesis = await apiRequest(path, {
       text: job.translated_text,
-      voiceId: '21m00Tcm4TlvDq8ikWAM'
+      voiceId: job.voice_id
     });
 
     const { data: translatedAudioUrl } = await synthesis;
