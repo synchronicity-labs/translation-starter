14576cbec1078fa503f2840f06ad31854abfabbd
Noah Milberger <n.milberger@gmail.com>
2023-11-18 00:38:36 -0500

    Created useJobData for handling jobs.

---
+++ hooks/useJobData.ts
@@ -0,0 +1,182 @@
+'use client';
+
+import { Job, JobStatus } from '@/types/db';
+import supabase from '@/utils/supabase';
+import synchronize from '@/utils/synchronize';
+import synthesisSpeech from '@/utils/sythesis-speech';
+import transcribe from '@/utils/transcribe';
+import translate from '@/utils/translate';
+import updateJob from '@/utils/update-job';
+import { useToast } from '@chakra-ui/react';
+import { PostgrestError } from '@supabase/supabase-js';
+import { useState, useEffect } from 'react';
+
+interface UseJobDataOutput {
+  jobs: Job[];
+  loading: boolean;
+  error: PostgrestError | null;
+}
+
+export default function useJobData(userId: string): UseJobDataOutput {
+  const [jobs, setJobs] = useState<Job[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<PostgrestError | null>(null);
+
+  const toast = useToast();
+
+  useEffect(() => {
+    async function fetchJobs() {
+      setLoading(true);
+      setError(null);
+      const { data: fetchedJobs, error: fetchError } = await supabase
+        .from('jobs')
+        .select('*')
+        .eq('user_id', userId) // Ensure `userId` is defined in your component
+        .neq('is_deleted', true);
+
+      if (fetchError) {
+        console.error('Error fetching jobs:', error);
+        setError(fetchError);
+      } else {
+        setJobs(fetchedJobs || []);
+      }
+      setLoading(false);
+    }
+
+    fetchJobs();
+  }, [userId]);
+
+  useEffect(() => {
+    const channel = supabase
+      .channel('realtime jobs')
+      .on(
+        'postgres_changes',
+        { event: 'INSERT', schema: 'public', table: 'jobs' },
+        (payload) => {
+          setJobs([...(jobs || []), payload.new as Job]);
+        }
+      )
+      .on(
+        'postgres_changes',
+        { event: 'UPDATE', schema: 'public', table: 'jobs' },
+        (payload) => {
+          setJobs((currentJobs) => {
+            if (payload.new.is_deleted || payload.new.status === 'failed') {
+              return currentJobs.filter((job) => job.id !== payload.new.id);
+            }
+            // Find the index of the job that has been updated.
+            const jobIndex = currentJobs.findIndex(
+              (job) => job.id === payload.new.id
+            );
+
+            // If the job doesn't exist, return the current state without any changes.
+            if (jobIndex === -1) {
+              return currentJobs;
+            }
+
+            // If the job does exist, create a new array with the updated job.
+            const updatedJobs = [...currentJobs];
+            updatedJobs[jobIndex] = payload.new as Job;
+
+            // Return the updated jobs array.
+            return updatedJobs;
+          });
+        }
+      )
+      .subscribe();
+
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, [supabase, jobs, setJobs]);
+
+  useEffect(() => {
+    for (const job of jobs) {
+      switch (job.status) {
+        case 'uploading':
+          if (job.original_video_url && job.original_audio_url) {
+            updateJob(job, { status: 'transcribing' }, () =>
+              handleJobFailed(job.id, 'Failed to update job to transcribing')
+            );
+            console.log('intiating transcription');
+            transcribe(job, () =>
+              handleJobFailed(job.id, 'Failed to transcribe')
+            );
+          }
+          break;
+        case 'transcribing':
+          if (job.transcript && job.transcription_id) {
+            updateJob(job, { status: 'translating' }, () =>
+              handleJobFailed(
+                job.id,
+                'Failed to update job status to translating'
+              )
+            );
+            translate(job, () =>
+              handleJobFailed(job.id, 'Failed to translate')
+            );
+          }
+          break;
+        case 'translating':
+          if (job.translated_text) {
+            updateJob(job, { status: 'synthesizing' }, () =>
+              handleJobFailed(
+                job.id,
+                'Failed to update job status to synthesizing'
+              )
+            );
+            synthesisSpeech(job, () =>
+              handleJobFailed(job.id, 'Failed to synthesize speech')
+            );
+          }
+          break;
+        case 'synthesizing':
+          if (job.translated_audio_url) {
+            updateJob(job, { status: 'synchronizing' }, () =>
+              handleJobFailed(
+                job.id,
+                'Failed to update job status to synchronizing'
+              )
+            );
+            synchronize(job, () =>
+              handleJobFailed(job.id, 'Failed to synchronize')
+            );
+          }
+          break;
+        case 'synchronizing':
+          if (job.video_url) {
+            updateJob(job, { status: 'completed' }, () =>
+              handleJobFailed(
+                job.id,
+                'Failed to update job status to completed'
+              )
+            );
+          }
+          break;
+        default:
+          break;
+      }
+    }
+  }, [jobs]);
+
+  const handleJobFailed = async (jobId: string, errorMessage: string) => {
+    await fetch('/api/db/update-job', {
+      method: 'POST',
+      body: JSON.stringify({
+        jobId,
+        updatedFields: {
+          status: 'failed' as JobStatus
+        }
+      })
+    });
+    toast({
+      title: 'Error occured while creating video',
+      description: errorMessage,
+      status: 'error',
+      duration: null,
+      isClosable: true
+    });
+  };
+
+  return { jobs, loading, error };
+}
