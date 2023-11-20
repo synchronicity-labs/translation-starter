21b5537edec52d7239a42f07a9a0788f556a5b9c
Noah Milberger <n.milberger@gmail.com>
2023-11-20 08:41:52 -0500

    Cleaned up and added better error handling to lip-sync route.

--- app/api/lip-sync/route.ts
+++ app/api/lip-sync/route.ts
@@ -1,45 +1,110 @@
+import { isValidUrl, exists } from '@/utils/helpers';
+
 export async function POST(req: Request) {
-  if (req.method !== 'POST') {
-    return new Response('Method Not Allowed', {
-      headers: { Allow: 'POST' },
-      status: 405
-    });
+  // Ensure the API key is set
+  const syncLabsApiKey = process.env.SYNC_LABS_API_KEY;
+  if (!syncLabsApiKey) {
+    return new Response(
+      JSON.stringify({
+        error: { statusCode: 500, message: 'Server configuration error' }
+      }),
+      { status: 500 }
+    );
   }
+
+  // Ensure the method is POST
+  if (req.method !== 'POST') {
+    return new Response(
+      JSON.stringify({
+        error: { statusCode: 405, message: 'Method Not Allowed' }
+      }),
+      { status: 405 }
+    );
+  }
+
   const { videoUrl, audioUrl } = await req.json();
 
-  console.log('videoUrl: ', videoUrl);
-  console.log('audioUrl: ', audioUrl);
+  // Check if the values exist
+  if (!exists(videoUrl) || !exists(audioUrl)) {
+    return new Response(
+      JSON.stringify({
+        error: {
+          statusCode: 400,
+          message: 'Missing videoUrl or audioUrl in the request body.'
+        }
+      }),
+      { status: 400 }
+    );
+  }
+
+  // Validate URLs after confirming they exist
+  if (!isValidUrl(videoUrl) || !isValidUrl(audioUrl)) {
+    return new Response(
+      JSON.stringify({
+        error: {
+          statusCode: 400,
+          message: 'Invalid URL provided'
+        }
+      }),
+      { status: 400 }
+    );
+  }
 
   const baseUrl =
     process.env.NEXT_PUBLIC_SITE_URL ||
-    'https://c23a-2601-19c-4400-f7f0-00-4b36.ngrok-free.app';
-  const response = await fetch(`https://api.synclabs.so/video`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-API-KEY': process.env.SYNC_LABS_API_KEY as string
-    },
-    body: JSON.stringify({
-      audioUrl,
-      videoUrl,
-      synergize: true,
-      webhookUrl: `${baseUrl}/api/lip-sync/webhook`
-    })
-  });
+    ' https://9ee2-2601-19c-4400-f7f0-00-4b36.ngrok-free.app';
 
-  if (!response.ok || !response.body) {
-    console.error(
-      `Failed to lip sync video to audio: ${response.status} ${response.statusText}`
-    );
-    return new Response(JSON.stringify({ error: { statusCode: 500 } }), {
-      status: 500
+  // Try to send the request to SyncLabs
+  try {
+    // Send the request to SyncLabs
+    const response = await fetch(`https://api.synclabs.so/video`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-API-KEY': syncLabsApiKey
+      },
+      body: JSON.stringify({
+        audioUrl,
+        videoUrl,
+        synergize: true,
+        webhookUrl: `${baseUrl}/api/lip-sync/webhook`
+      })
     });
+
+    // Handle errors
+    if (!response.ok) {
+      const errorText = await response.text();
+      console.error(
+        `Failed to lip sync video to audio: ${response.status} ${errorText}`
+      );
+      return new Response(
+        JSON.stringify({
+          error: {
+            statusCode: response.status,
+            message: errorText
+          }
+        }),
+        { status: response.status }
+      );
+    }
+
+    // Return the response
+    const data = await response.json();
+
+    return new Response(JSON.stringify({ data }), {
+      status: 200
+    });
+  } catch (error) {
+    // Handle unexpected errors
+    console.error(`Unexpected error occurred: ${error}`);
+    return new Response(
+      JSON.stringify({
+        error: {
+          statusCode: 500,
+          message: 'Unexpected error occurred'
+        }
+      }),
+      { status: 500 }
+    );
   }
-
-  const data = await response.json();
-  console.log('data: ', data);
-
-  return new Response(JSON.stringify({ data }), {
-    status: 200
-  });
 }
