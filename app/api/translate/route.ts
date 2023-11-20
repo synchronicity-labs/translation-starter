721783edfd8e72bb54bebcb2dfa90b7a36406d0b
Noah Milberger <n.milberger@gmail.com>
2023-11-20 08:43:21 -0500

    Cleaned up and added better error handling to translate route.

--- app/api/translate/route.ts
+++ app/api/translate/route.ts
@@ -1,19 +1,35 @@
+import { exists } from '@/utils/helpers';
 import { OpenAI } from 'openai';
 
 const openai = new OpenAI();
 
 export async function POST(req: Request) {
+  // Ensure the method is POST
   if (req.method !== 'POST') {
-    return new Response('Method Not Allowed', {
-      headers: { Allow: 'POST' },
-      status: 405
-    });
+    return new Response(
+      JSON.stringify({
+        error: { statusCode: 405, message: 'Method Not Allowed' }
+      }),
+      { status: 405 }
+    );
   }
 
   const { text, language } = await req.json();
 
+  // Check if the values exist
+  if (!exists(text) || !exists(language)) {
+    return new Response(
+      JSON.stringify({
+        error: {
+          statusCode: 400,
+          message: 'Missing text or language in the request body.'
+        }
+      }),
+      { status: 400 }
+    );
+  }
+
   try {
-    console.log(`starting translation on: ${text}`);
     const completion = await openai.chat.completions.create({
       model: 'gpt-4',
       messages: [
@@ -23,20 +39,25 @@ export async function POST(req: Request) {
         },
         {
           role: 'user',
-          content: `Translate the following text with high accuracy into ${language}: '${text}'. Please focus on preserving the original meaning and consider cultural nuances.`
+          content: `Translate the following text with high accuracy into ${language}. Please focus on preserving the original meaning and consider cultural nuances. Text to translate - '${text}'.`
         }
       ]
     });
     const translation = completion.choices[0].message.content;
 
-    console.log('translation: ', translation);
     return new Response(JSON.stringify({ data: translation }), {
       status: 200
     });
   } catch (error) {
-    console.error(`Failed to translate text: `, error);
-    return new Response(JSON.stringify({ error: { statusCode: 500 } }), {
-      status: 500
-    });
+    console.error(`Unexpected error occurred: ${error}`);
+    return new Response(
+      JSON.stringify({
+        error: {
+          statusCode: 500,
+          message: 'Unexpected error occurred'
+        }
+      }),
+      { status: 500 }
+    );
   }
 }
