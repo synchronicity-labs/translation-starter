49dcdc775b5f5dfdb90fc1d0e54d54d830505f08
Noah Milberger <n.milberger@gmail.com>
2023-11-20 08:43:04 -0500

    Cleaned up and added better error handling to transcribe route.

--- app/api/transcribe/route.ts
+++ app/api/transcribe/route.ts
@@ -1,50 +1,114 @@
+import { exists, isValidUrl } from '@/utils/helpers';
 import fetch, { FormData } from 'node-fetch';
 
 export async function POST(req: Request) {
-  console.log('start transcription');
+  // Ensure the API key is set
+  const gladiaApiKey = process.env.GLADIA_API_KEY;
+  if (!gladiaApiKey) {
+    return new Response(
+      JSON.stringify({
+        error: { statusCode: 500, message: 'Server configuration error' }
+      }),
+      { status: 500 }
+    );
+  }
+
+  // Ensure the method is POST
   if (req.method !== 'POST') {
-    return new Response('Method Not Allowed', {
-      headers: { Allow: 'POST' },
-      status: 405
-    });
+    return new Response(
+      JSON.stringify({
+        error: { statusCode: 405, message: 'Method Not Allowed' }
+      }),
+      { status: 405 }
+    );
   }
 
   const { url } = await req.json();
 
+  // Check if the values exist
+  if (!exists(url)) {
+    return new Response(
+      JSON.stringify({
+        error: {
+          statusCode: 400,
+          message: 'Missing videoUrl or audioUrl in the request body.'
+        }
+      }),
+      { status: 400 }
+    );
+  }
+
+  // Validate URLs after confirming they exist
+  if (!isValidUrl(url)) {
+    return new Response(
+      JSON.stringify({
+        error: {
+          statusCode: 400,
+          message: 'Invalid URL provided'
+        }
+      }),
+      { status: 400 }
+    );
+  }
+
   const baseUrl =
     process.env.NEXT_PUBLIC_SITE_URL ||
-    'https://c23a-2601-19c-4400-f7f0-00-4b36.ngrok-free.app';
+    'https://9ee2-2601-19c-4400-f7f0-00-4b36.ngrok-free.app';
 
   const webhook_url = `${baseUrl}/api/transcribe/webhook`;
 
-  const form = new FormData();
-  form.append('audio_url', url);
-  form.append('webhook_url', webhook_url);
+  // Try to send the request to Gladia
+  try {
+    const form = new FormData();
+    form.append('audio_url', url);
+    form.append('webhook_url', webhook_url);
 
-  const response = await fetch(
-    'https://api.gladia.io/audio/text/audio-transcription/',
-    {
-      method: 'POST',
-      headers: {
-        accept: 'application/json',
-        'x-gladia-key': process.env.GLADIA_API_KEY as string
-      },
-      body: form
-    }
-  );
-
-  if (!response.ok) {
-    console.error(
-      `Failed to transcribe audio to text: ${response.status} ${response.statusText}`
+    // Send the request to Gladia
+    const response = await fetch(
+      'https://api.gladia.io/audio/text/audio-transcription/',
+      {
+        method: 'POST',
+        headers: {
+          accept: 'application/json',
+          'x-gladia-key': gladiaApiKey
+        },
+        body: form
+      }
     );
-    return new Response(JSON.stringify({ error: { statusCode: 500 } }), {
-      status: 500
+
+    // Handle errors
+    if (!response.ok) {
+      const errorText = await response.text();
+      console.error(
+        `Failed to transcribe audio to text: ${response.status} ${response.statusText}`
+      );
+      return new Response(
+        JSON.stringify({
+          error: { statusCode: response.status, message: errorText }
+        }),
+        {
+          status: response.status
+        }
+      );
+    }
+
+    // Return the response
+    const data = await response.json();
+
+    return new Response(JSON.stringify({ data }), {
+      status: 200
     });
+  } catch (error) {
+    // Handle unexpected , message
+    console.error(`Unexpected error occurred: ${error}`);
+    return new Response(
+      JSON.stringify({
+        error: {
+          statusCode: 500,
+          message: 'Unexpected error occurred'
+        }
+      }),
+      { status: 500 }
+    );
   }
-
-  const data = await response.json();
-
-  return new Response(JSON.stringify(data), {
-    status: 200
-  });
 }
