cf8fda6ca8ef281a293e42d7f57368b26e39b3a3
Noah Milberger <n.milberger@gmail.com>
2024-01-09 15:25:25 -0500

    Moved langauges into a new data folder.

---
+++ data/languages.ts
@@ -0,0 +1,39 @@
+interface Language {
+  name: string;
+  code: string;
+}
+
+export const languages = [
+  { name: 'English', code: 'en' },
+  { name: 'Mandarin Chinese', code: 'zh' },
+  { name: 'Hindi', code: 'hi' },
+  { name: 'Spanish', code: 'es' },
+  { name: 'French', code: 'fr' },
+  { name: 'Arabic', code: 'ar' },
+  { name: 'Portuguese', code: 'pt' },
+  { name: 'Russian', code: 'ru' },
+  { name: 'Indonesian', code: 'id' },
+  { name: 'German', code: 'de' },
+  { name: 'Japanese', code: 'ja' },
+  { name: 'Korean', code: 'ko' },
+  { name: 'Vietnamese', code: 'vi' },
+  { name: 'Italian', code: 'it' },
+  { name: 'Dutch', code: 'nl' },
+  { name: 'Turkish', code: 'tr' },
+  { name: 'Polish', code: 'pl' },
+  { name: 'Swedish', code: 'sv' },
+  { name: 'Filipino', code: 'tl' },
+  { name: 'Malay', code: 'ms' },
+  { name: 'Romanian', code: 'ro' },
+  { name: 'Ukrainian', code: 'uk' },
+  { name: 'Greek', code: 'el' },
+  { name: 'Czech', code: 'cs' },
+  { name: 'Danish', code: 'da' },
+  { name: 'Finnish', code: 'fi' },
+  { name: 'Bulgarian', code: 'bg' },
+  { name: 'Croatian', code: 'hr' },
+  { name: 'Slovak', code: 'sk' },
+  { name: 'Hungarian', code: 'hu' },
+  { name: 'Norwegian', code: 'no' },
+  { name: 'Tamil', code: 'ta' }
+];
