ca097332a33ad1e0f89aa61655d9a2c501133474
Noah Milberger <n.milberger@gmail.com>
2024-01-29 01:24:01 -0800

    More logs

--- app/api/lip-sync/route.ts
+++ app/api/lip-sync/route.ts
@@ -1,3 +1,5 @@
+import { infer } from './../../../node_modules/inngest/helpers/validators/zod.d';
+import { SynchronicityLogger } from '@/lib/SynchronicityLogger';
 import { isValidUrl, exists } from '@/utils/helpers';
 
 const baseUrl = process.env.NEXT_PUBLIC_SITE_URL;
@@ -6,11 +8,16 @@ if (!baseUrl) {
   throw new Error('NEXT_PUBLIC_SITE_URL is not set');
 }
 
+const logger = new SynchronicityLogger({
+  name: 'api/lip-sync/route'
+});
+
 const API_URL =
   process.env.NEXT_PUBLIC_SYNC_API_URL || 'https://api.synclabs.so';
 
 export async function POST(req: Request) {
   // Ensure the API key is set
+  logger.log('Checking for SyncLabs API key');
   const syncLabsApiKey = process.env.SYNC_LABS_API_KEY;
   if (!syncLabsApiKey) {
     return new Response(
@@ -20,9 +27,11 @@ export async function POST(req: Request) {
       { status: 500 }
     );
   }
+  logger.log('SyncLabs API key found');
 
   // Ensure the method is POST
   if (req.method !== 'POST') {
+    logger.error(`Method Not Allowed: ${req.method}`);
     return new Response(
       JSON.stringify({
         error: { statusCode: 405, message: 'Method Not Allowed' }
@@ -35,6 +44,7 @@ export async function POST(req: Request) {
 
   // Check if the values exist
   if (!exists(videoUrl) || !exists(audioUrl)) {
+    logger.error('Missing videoUrl or audioUrl in the request body.');
     return new Response(
       JSON.stringify({
         error: {
@@ -45,6 +55,7 @@ export async function POST(req: Request) {
       { status: 400 }
     );
   }
+  logger.log`Video and audio exist`;
 
   // Validate URLs after confirming they exist
   if (!isValidUrl(videoUrl) || !isValidUrl(audioUrl)) {
@@ -59,10 +70,11 @@ export async function POST(req: Request) {
     );
   }
 
-  // Try to send the request to SyncLabs
+  logger.log('Sending request to SyncLabs');
   try {
     // Send the request to SyncLabs
     // const response = await fetch(`https://api.synclabs.so/video`, {
+    logger.log('Sending request to SyncLabs at ' + API_URL);
     const response = await fetch(API_URL, {
       method: 'POST',
       headers: {
@@ -77,11 +89,12 @@ export async function POST(req: Request) {
         model: 'sync-1.5-beta'
       })
     });
+    logger.log('Response from SyncLabs received');
 
     // Handle errors
     if (!response.ok) {
       const errorText = await response.text();
-      console.error(
+      logger.error(
         `Failed to lip sync video to audio: ${response.status} ${errorText}`
       );
       return new Response(
@@ -97,13 +110,14 @@ export async function POST(req: Request) {
 
     // Return the response
     const data = await response.json();
+    logger.log('Returning response from SyncLabs');
 
     return new Response(JSON.stringify({ data }), {
       status: 200
     });
   } catch (error) {
     // Handle unexpected errors
-    console.error(`Unexpected error occurred: ${error}`);
+    logger.error(`Unexpected error occurred: ${error}`);
     return new Response(
       JSON.stringify({
         error: {
