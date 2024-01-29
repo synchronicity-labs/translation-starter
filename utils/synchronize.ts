cc86327623ef7db59d9cd42196eaa0877246d7f5
Noah Milberger <n.milberger@gmail.com>
2024-01-29 01:33:01 -0800

    In-line synchronize api call

--- utils/synchronize.ts
+++ utils/synchronize.ts
@@ -6,27 +6,68 @@ const logger = new SynchronicityLogger({
   name: 'utils/synchronize'
 });
 
-const TRANSLATION_API = process.env.NEXT_PUBLIC_TRANSLATION_API;
+const baseUrl = process.env.NEXT_PUBLIC_SITE_URL;
+
+if (!baseUrl) {
+  throw new Error('NEXT_PUBLIC_SITE_URL is not set');
+}
+const API_URL =
+  process.env.NEXT_PUBLIC_SYNC_API_URL || 'https://api.synclabs.so';
 
 export default async function synchronize(job: Job) {
-  const path = `${TRANSLATION_API}/api/lip-sync`;
-  const response = await fetch(path, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      videoUrl: job.original_video_url,
-      audioUrl: job.translated_audio_url
-    })
-  });
-  if (!response.ok) {
-    const text = await response.text();
-    logger.error(
-      `Failed to synchronize job (${path}) ${job.id} - ${text} - ${response.status} - ${response.statusText}`
-    );
-    const err = new Error();
-    const stack = err.stack;
-    throw new Error(
-      `HTTP error! - status=${response.status} - text=${text} - stack=\n${stack}`
+  logger.log('Checking for SyncLabs API key');
+  const syncLabsApiKey = process.env.SYNC_LABS_API_KEY;
+  if (!syncLabsApiKey) {
+    return new Response(
+      JSON.stringify({
+        error: { statusCode: 500, message: 'Server configuration error' }
+      }),
+      { status: 500 }
     );
   }
+  logger.log('SyncLabs API key found');
+
+  logger.log('Sending request to SyncLabs');
+  // Send the request to SyncLabs
+  // const response = await fetch(`https://api.synclabs.so/video`, {
+  logger.log('Sending request to SyncLabs at ' + API_URL);
+  const response = await fetch(API_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-API-KEY': syncLabsApiKey
+    },
+    body: JSON.stringify({
+      audioUrl: job.translated_audio_url,
+      videoUrl: job.original_video_url,
+      synergize: true,
+      webhookUrl: `${baseUrl}/api/lip-sync/webhook`,
+      model: 'sync-1.5-beta'
+    })
+  });
+  logger.log('Response from SyncLabs received');
+
+  // Handle errors
+  if (!response.ok) {
+    const errorText = await response.text();
+    logger.error(
+      `Failed to lip sync video to audio: ${response.status} ${errorText}`
+    );
+    return new Response(
+      JSON.stringify({
+        error: {
+          statusCode: response.status,
+          message: errorText
+        }
+      }),
+      { status: response.status }
+    );
+  }
+
+  // Return the response
+  const data = await response.json();
+  // TODO: store the sync-labs job id in the database, so we can allow
+  // users to poll the endpoint to check the status of the job
+  // otherwise they hit http connection timeout errors
+  logger.log('Job queued successfully');
 }
